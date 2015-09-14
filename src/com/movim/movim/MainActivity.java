56f695e099ae040418c16038cf4c15691921b482
Jaussoin Timothée <edhelas@movim.eu>
2015-09-14 17:01:47 +0200

    Use Android notifications Add a Pod chooser when launching the App

--- src/com/movim/movim/MainActivity.java
+++ src/com/movim/movim/MainActivity.java
@@ -1,64 +1,84 @@
 package com.movim.movim;
 
 import android.annotation.SuppressLint;
-import android.annotation.TargetApi;
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.net.Uri;
+import android.net.http.SslError;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.Window;
+import android.webkit.SslErrorHandler;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ProgressBar;
+import android.widget.Toast;
+import android.webkit.JavascriptInterface;
 
-@SuppressLint("SetJavaScriptEnabled") public class MainActivity extends Activity {
-	private WebView webview;
-	private ProgressBar progressbar;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+@SuppressLint("SetJavaScriptEnabled")
+public class MainActivity extends Activity {
+    private WebView webview;
+    private ProgressBar progressbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-    	super.onCreate(savedInstanceState);
-    	getWindow().requestFeature(Window.FEATURE_PROGRESS);
-    	getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_PROGRESS);
+        getWindow().requestFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
 
-        webview = (WebView)findViewById(R.id.webview);
-        
+        webview = (WebView) findViewById(R.id.webview);
+
         webview.getSettings().setJavaScriptEnabled(true);
         webview.getSettings().setDomStorageEnabled(true);
         webview.getSettings().setMixedContentMode(0);
 
-        progressbar = (ProgressBar)findViewById(R.id.progress);
+        progressbar = (ProgressBar) findViewById(R.id.progress);
         progressbar.setIndeterminate(true);
-        
-		if(Build.VERSION.SDK_INT >= 21) {
-			getWindow().setStatusBarColor(Color.rgb(48, 63, 159));
-		}
+
+        if (Build.VERSION.SDK_INT >= 21) {
+            getWindow().setStatusBarColor(Color.rgb(48, 63, 159));
+        }
+
+        webview.addJavascriptInterface(this, "Android");
 
         webview.setWebChromeClient(new WebChromeClient() {
             public void onProgressChanged(WebView view, int progress) {
-               progressbar.setProgress(progress);
-               if(progress < 100 && progress > 0 && progressbar.getVisibility() == ProgressBar.GONE){
-            	   progressbar.setIndeterminate(true);
-                   progressbar.setVisibility(ProgressBar.VISIBLE);
-               }
-               if(progress == 100) {
-            	   progressbar.setVisibility(ProgressBar.GONE);
-               }
-               
+                progressbar.setProgress(progress);
+                if (progress < 100 && progress > 0 && progressbar.getVisibility() == ProgressBar.GONE) {
+                    progressbar.setIndeterminate(true);
+                    progressbar.setVisibility(ProgressBar.VISIBLE);
+                }
+                if (progress == 100) {
+                    progressbar.setVisibility(ProgressBar.GONE);
+                }
+
             }
-         
+
         });
         webview.setWebViewClient(new WebViewClient() {
             public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
-        		webview.loadUrl("file:///android_asset/error.html");
-        	}
+                webview.loadUrl("file:///android_asset/error.html");
+            }
+
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                if (url != null && url.startsWith("https://pod.movim.eu/")) {
+                String origin = Uri.parse(view.getUrl()).getHost();
+                String aim = Uri.parse(url).getHost();
+
+                if(origin.isEmpty() || origin.equals(aim)) {
                     return false;
                 } else {
                     Intent intent = new Intent(Intent.ACTION_VIEW);
@@ -67,18 +87,58 @@ import android.widget.ProgressBar;
                     return true;
                 }
             }
+
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                handler.proceed(); // Ignore SSL certificate errors
+            }
         });
 
-        webview.loadUrl("https://pod.movim.eu/");
+        webview.loadUrl("file:///android_asset/index.html");
     }
-    
+
+    @JavascriptInterface
+    public void showToast(String toast) {
+        Toast.makeText(this, toast, Toast.LENGTH_SHORT).show();
+    }
+    @JavascriptInterface
+    public void showNotification(String title, String body, String picture) {
+        Bitmap pictureBitmap = getBitmapFromURL(picture);
+        PendingIntent pi = PendingIntent.getActivity(this, 0, new Intent(this, MainActivity.class), 0);
+        //Drawable d = getPackageManager().getApplicationIcon(getApplicationInfo());
+        Notification notification = new Notification.Builder(this)
+                .setSmallIcon(android.R.drawable.sym_action_chat)
+                .setLargeIcon(pictureBitmap)
+                .setContentTitle(title)
+                .setContentText(body)
+                .setContentIntent(pi)
+                .setAutoCancel(true)
+                .build();
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(0, notification);
+    }
+
+    public static Bitmap getBitmapFromURL(String src) {
+        try {
+            URL url = new URL(src);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream input = connection.getInputStream();
+            Bitmap myBitmap = BitmapFactory.decodeStream(input);
+            return myBitmap;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
     public boolean onKeyUp(int keyCode, KeyEvent event) {
         if (keyCode == KeyEvent.KEYCODE_MENU) {
-        	webview.loadUrl("javascript:MovimTpl.toggleMenu()");
+            webview.loadUrl("javascript:MovimTpl.toggleMenu()");
             return true;
         }
         if (keyCode == KeyEvent.KEYCODE_BACK) {
-        	webview.loadUrl("javascript:MovimTpl.back()");
+            webview.loadUrl("javascript:MovimTpl.back()");
             return true;
         }
         return super.onKeyUp(keyCode, event);
